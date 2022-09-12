0b9aabcf7516150e1e15af5d618709d6143e0d7d
Shreya Shah <shreyashah690@gmail.com>
2022-09-12 19:03:23 -0400

    Updated input to char array in Token class.

--- src/edu/ufl/cise/plpfa22/Token.java
+++ src/edu/ufl/cise/plpfa22/Token.java
@@ -2,12 +2,12 @@ package edu.ufl.cise.plpfa22;
 
 public class Token implements IToken {
     private final Kind kind;
-    private final String input;
+    private final char[] input;
     private final int position;
     private final int len;
     private final SourceLocation sourceLocation;
 
-    public Token(Kind kind, String input, int position, int len, SourceLocation sourceLocation) {
+    public Token(Kind kind, char[] input, int position, int len, SourceLocation sourceLocation) {
         this.kind = kind;
         this.input = input;
         this.position = position;
@@ -22,7 +22,7 @@ public class Token implements IToken {
 
     @Override
     public char[] getText() {
-        return input.substring(position, len).toCharArray();
+        return String.copyValueOf(input, position, len).toCharArray();
     }
 
     @Override
@@ -33,18 +33,18 @@ public class Token implements IToken {
     @Override
     public int getIntValue() {
         assert getKind() == Kind.NUM_LIT;
-        return Integer.parseInt(input.substring(position, len));
+        return Integer.parseInt(String.copyValueOf(input, position, len));
     }
 
     @Override
     public boolean getBooleanValue() {
         assert getKind() == Kind.BOOLEAN_LIT;
-        return Boolean.parseBoolean(input.substring(position, len));
+        return Boolean.parseBoolean(String.copyValueOf(input, position, len));
     }
 
     @Override
     public String getStringValue() {
         assert getKind() == Kind.STRING_LIT;
-        return input.substring(position, len);
+        return String.copyValueOf(input, position, len);
     }
 }
